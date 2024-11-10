cd4ddd1e50fbbd683e71b1f0b9f45bbe4b1b9941
brent hartshorn <brenthartshorn@proton.me>
2024-11-10 11:14:12 -0800

    blender to json

--- libglzag.py
+++ libglzag.py
@@ -1,4 +1,55 @@
-import sys
+import os, sys, json, subprocess
+
+def mesh_to_json(ob):
+	verts = []
+	faces = []
+	dump = {
+		'verts':verts,
+		'faces':faces,
+	}
+	for v in ob.data.vertices:
+		verts += list(v.co)
+	return dump
+
+
+def blend_to_json(name):
+	import bpy
+	assert name in bpy.data.objects
+	dump = {}
+	ob = bpy.data.objects[name]
+	if ob.type=="MESH":
+		dump.update( mesh_to_json(ob) )
+	return dump
+
+out = None
+obj = None
+for arg in sys.argv:
+	if arg.startswith('--json='):
+		out = arg.split('=')[-1]
+	if arg.startswith('--dump='):
+		obj = arg.split('=')[-1]
+
+if out:
+	assert obj
+	dump = blend_to_json(obj)
+	open(out,'wb').write(
+		json.dumps(dump).encode('utf-8')
+	)
+	sys.exit()
+
+
+if sys.platform == 'win32':
+	BLENDER = 'C:/Program Files/Blender Foundation/Blender 4.2/blender.exe'
+	if not os.path.isfile(BLENDER):
+		BLENDER = 'C:/Program Files/Blender Foundation/Blender 3.6/blender.exe'
+elif sys.platform == 'darwin':
+	BLENDER = '/Applications/Blender.app/Contents/MacOS/Blender'
+else:
+	BLENDER = 'blender'
+	if os.path.isfile(os.path.expanduser('~/Downloads/blender-4.2.1-linux-x64/blender')):
+		BLENDER = os.path.expanduser('~/Downloads/blender-4.2.1-linux-x64/blender')
+
+
 import numpy as np
 from OpenGL.GL import GL_COLOR_BUFFER_BIT, GL_FLOAT, GL_TRIANGLES, glClear, glClearColor, glDrawArrays
 
@@ -74,9 +125,21 @@ class Viewer(QOpenGLWidget):
 		self.program.enableAttributeArray("aPosition")
 		glDrawArrays(GL_TRIANGLES, 0, 3)
 
+
+	def view_blender_object(self, name, blend):
+		cmd = [BLENDER, blend, '--background', '--python', __file__, '--', '--json=/tmp/__object__.json', '--dump=%s' % name]
+		print(cmd)
+		subprocess.check_call(cmd)
+		ob = json.loads(open('/tmp/__object__.json').read())
+		print('got json:', ob)
+
+
 if __name__ == "__main__":
 	QApplication.setAttribute(Qt.ApplicationAttribute.AA_UseDesktopOpenGL)
 	app = QApplication(sys.argv)
 	w = Viewer()
+	for arg in sys.argv:
+		if arg.endswith('.blend'):
+			w.view_blender_object('Cube', arg)
 	w.show()
 	sys.exit(app.exec())
