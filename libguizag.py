a65b0f6e78b2a79b1fe766d0ad25b72ce6fe4a7f
brent hartshorn <brenthartshorn@proton.me>
2024-11-24 07:04:35 -0800

    moved LearnC3 to learn_c3.py

--- libguizag.py
+++ libguizag.py
@@ -33,13 +33,6 @@ def ensure_megasolid():
 		print(cmd)
 		subprocess.check_call(cmd.split())
 ensure_megasolid()
-C3_LEARN = os.path.join(_thisdir,'c3-learn')
-def ensure_c3_learn():
-	if not os.path.isdir(C3_LEARN):
-		cmd = 'git clone --depth 1 https://github.com/brentharts/c3-learn.git'
-		print(cmd)
-		subprocess.check_call(cmd.split())
-ensure_c3_learn()
 
 
 if os.path.isdir(MEGASOLID):
@@ -98,6 +91,7 @@ else:
 		QIcon,
 	)
 
+import learn_c3
 
 if sys.platform == 'win32':
 	C3 = os.path.join(_thisdir,'c3/c3c.exe')  ## latest unstable
@@ -199,7 +193,7 @@ class ZigZagEditor( MegasolidCodeEditor ):
 			layout.addWidget(self.materials_container)
 			#layout.addLayout(self.materials_layout)
 
-			self.learn_c3_widget = LearnC3(zoomout=3)
+			self.learn_c3_widget = learn_c3.LearnC3(zoomout=3)
 			self.learn_c3_widget.setStyleSheet('background-color:white; color:black')
 			layout.addWidget(self.learn_c3_widget)
 
@@ -1531,6 +1525,9 @@ class BlendWrap:
 '''
 
 
+
+
+
 LEARN_C3 = [
 """
 .c3.script = '''
@@ -1546,121 +1543,6 @@ fn void onclick( int x, int y ) @extern("onclick") @wasm {
 """,
 
 ]
-class LearnC3(QWidget):
-	def __init__(self, zoomout=0):
-		super().__init__()
-		self.zoomout = zoomout
-		self.resize(640, 700)
-		self.setWindowTitle('Learn C3')
-		self.main_vbox = vbox = QVBoxLayout()
-		self.setLayout(self.main_vbox)
-		self.mds = []
-		path = os.path.join(C3_LEARN, 'old/content/Basics')
-		for md in os.listdir(path):
-			if md.endswith('.md'):
-				self.mds.append( os.path.join(path, md) )
-		path = os.path.join(C3_LEARN, 'old/content/More')
-		for md in os.listdir(path):
-			if md.endswith('.md'):
-				self.mds.append( os.path.join(path, md) )
-		path = os.path.join(C3_LEARN, 'old/content/Try')
-		for md in os.listdir(path):
-			if md.endswith('.md'):
-				self.mds.append( os.path.join(path, md) )
-
-		self.load_random()
-		self.show()
-
-	def load_random(self):
-		clear_layout(self.main_vbox)
-		md = choice(self.mds)
-		print('loading:', md)
-		self.setWindowTitle(md)
-		md = open(md).read()
-		html = self.parse_md(md)
-		self.edit = edit = QTextEdit()
-		edit.setHtml(html)
-		if self.zoomout:
-			edit.zoomOut(self.zoomout)
-		self.main_vbox.addWidget(edit)
-
-		btn = QPushButton('next', self.edit)
-		btn.clicked.connect(lambda b: self.load_random())
-
-	def load(self, tag):
-		tag = os.path.sep + tag
-		clear_layout(self.main_vbox)
-		ok = None
-		for md in self.mds:
-			if md.endswith(tag):
-				ok = md
-				break
-		if ok:
-			self.setWindowTitle(md)
-			md = open(md).read()
-			html = self.parse_md(md)
-			self.edit = edit = QTextEdit()
-			edit.setHtml(html)
-			if self.zoomout:
-				edit.zoomOut(self.zoomout)
-			self.main_vbox.addWidget(edit)
-
-			btn = QPushButton('next', self.edit)
-			btn.clicked.connect(lambda b: self.load_random())
-
-	def parse_md(self, md):
-		o = []
-		in_backticks = False
-		for ln in md.splitlines():
-			print(ln)
-			if ln.startswith('title:'):
-				title = ln.split('title:')[-1].strip()
-				o.append('<h2>Example: %s</h2>' % title)
-				continue
-			elif ln.strip()=='---':
-				o.append('<hr/>')
-				continue
-			elif ln.strip()=='```':
-				if in_backticks:
-					in_backticks = False
-					o.append('</pre>')
-				else:
-					in_backticks = True
-					o.append('<pre style="background-color:lightgray">')
-				continue
-			elif ln.startswith('- '):
-				o.append('<span style="font-size:18px">•%s</span>' % ln)
-				continue
-			elif ln.startswith('weight:'):
-				continue
-			elif ln.startswith('{{<start>}}'):
-				continue
-			elif ln.startswith('{{<end'):
-				n = int(ln.split('{{<end')[-1].split('>')[0])
-				html = os.path.join(C3_LEARN, 'old/layouts/shortcodes/end%s.html' % n)
-				assert os.path.isfile(html)
-				for hl in open(html).read().splitlines():
-					if hl.strip().startswith('let defcod'):
-						code = hl[ hl.index('=')+1 : ].strip()
-						code = code.replace('\\t', '\t').replace('\\n', '\n')
-						if code.startswith('"') and code.endswith('";'):
-							code = code[1:-2]
-						code = code.replace('\\"', '"')
-						code = code.replace('<', '&lt;').replace('>', '&gt;')
-						o.append('<h3>C3 Code:</h3><pre>%s</pre>' % code)
-				continue
-			if in_backticks and ln.startswith('//'):
-				o.append('<i style="font-size:16px; background-color:gray; color:white">%s</i>' % ln)
-			elif in_backticks and ln.count('//')==1:
-				a,b = ln.split('//')
-				o.append('%s\t\t<i style="font-size:12px; background-color:gray; color:white">//%s</i>' % (a,b))
-			else:
-				o.append(ln)
-		return '<br/>'.join(o)
-
-
-
-
 
 class Window(QWidget):
 	def open_code_editor(self, *args):
@@ -2050,7 +1932,7 @@ if __name__=='__main__':
 		app.exec()
 	elif '--learn-c3' in sys.argv:
 		app = QApplication(sys.argv)
-		window = LearnC3()
+		window = learn_c3.LearnC3()
 		app.exec()
 	else:
 		main()
